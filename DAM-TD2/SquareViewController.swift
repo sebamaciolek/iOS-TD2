dfee2d8d049125d607e5618045935f01cd6fcca9
MACIOLEK Sebastian <ms613363@s202p06m.iutnice.unice.fr>
2017-01-10 15:45:55 +0100

    initial commit

---
+++ DAM-TD2/SquareViewController.swift
@@ -0,0 +1,51 @@
+//
+//  SquareViewController.swift
+//  DAM-TD2
+//
+//  Created by MACIOLEK Sebastian on 09/01/2017.
+//  Copyright © 2017 MACIOLEK Sebastian. All rights reserved.
+//
+
+import UIKit
+
+class SquareViewController: UIViewController {
+    var rouge = UIView()
+    var bleu = UIView()
+    var vert = UIView()
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+        
+        rouge.frame = CGRect.init(x: 10, y: 50, width: 300, height: 110)
+        rouge.backgroundColor = UIColor.red
+        rouge.center = self.view.center
+        self.view.addSubview(rouge)
+        
+        bleu.frame = CGRect.init(x: 70, y: 180, width: 50, height: 90)
+        bleu.backgroundColor = UIColor.blue
+        bleu.center = self.view.center
+        self.view.addSubview(bleu)
+        
+        vert.frame = CGRect.init(x: 60, y: 280, width: 220, height: 130)
+        vert.backgroundColor = UIColor.green
+        vert.center = self.view.center
+        self.view.addSubview(vert)
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+}
---
+++ ScrollViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ScrollViewController.swift
+//  DAM-TD2
+//
+//  Created by MACIOLEK Sebastian on 09/01/2017.
+//  Copyright © 2017 MACIOLEK Sebastian. All rights reserved.
+//
+
+import UIKit
+
+class ScrollViewController: UIScrollView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
