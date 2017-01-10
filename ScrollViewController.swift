aeef9c3190059e072e00dab1c04e3515db23ec33
MACIOLEK Sebastian <ms613363@s202p06m.iutnice.unice.fr>
2017-01-10 16:06:55 +0100

    classe ScrollViewController ajouté

--- ScrollViewController.swift
+++ ScrollViewController.swift
@@ -2,14 +2,16 @@
 //  ScrollViewController.swift
 //  DAM-TD2
 //
-//  Created by MACIOLEK Sebastian on 09/01/2017.
+//  Created by MACIOLEK Sebastian on 10/01/2017.
 //  Copyright © 2017 MACIOLEK Sebastian. All rights reserved.
 //
 
 import UIKit
 
-class ScrollViewController: UIScrollView {
+class ScrollViewController: UIViewController {
+    var view1 = UIView()
 
+    @IBOutlet weak var outletScrollView: UIScrollView!
     /*
     // Only override draw() if you perform custom drawing.
     // An empty implementation adversely affects performance during animation.
@@ -17,5 +19,21 @@ class ScrollViewController: UIScrollView {
         // Drawing code
     }
     */
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        view1.frame = CGRect.init(x: 10, y: 50, width: 300, height: 110)
+        view1.backgroundColor = UIColor.red
+        view1.center = self.view.center
+        outletScrollView.addSubview(view1)
+        
+    }
+
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
 
 }
